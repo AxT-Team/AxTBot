e841a00770e587f2ed9f92c2b9be85bdcec87b94
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-05-03 19:27:03 +0800

    Add if qqid is not int,return error.

--- utils/touch.py
+++ utils/touch.py
@@ -10,18 +10,23 @@ menu = '''使用方法:
 
 # ----------------主程序-----------------
 async def touch(qqid) -> None:
-    if qqid == "help":
-        return menu
-    elif qqid == "我":
-        return "很抱歉，由于技术限制，暂时不能这么用，请直接输入您的QQ号。"
+    try:
+        int(qqid)
+    except ValueError:
+        return "请输入正确的QQ号。"
     else:
-        api = api_url+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
-        response = requests.get(api)
-        message = json.loads(response.text)
-        if message['code'] == 429:
-            time.sleep(3)
+        if qqid == "help":
+            return menu
+        elif qqid == "我":
+            return "很抱歉，由于技术限制，暂时不能这么用，请直接输入您的QQ号。"
+        else:
+            api = api_url+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
             response = requests.get(api)
             message = json.loads(response.text)
-        return message['url']
+            if message['code'] == 429:
+                time.sleep(3)
+                response = requests.get(api)
+                message = json.loads(response.text)
+            return message['url']
         
 # ----------------主程序-----------------
