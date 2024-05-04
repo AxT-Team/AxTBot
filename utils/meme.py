083813eec47bd2aaaa5b99d3a3198c2eba767357
夜影星辰 <149694986+Yeying-Xingchen@users.noreply.github.com>
2024-05-04 16:16:35 +0800

    Add meme api.

--- utils/meme.py
+++ utils/meme.py
@@ -3,7 +3,7 @@ import requests
 import time
 
 # ------------------快速设置------------------
-api_url = "https://api.wer.plus/api/ruad?url=" # 请修改成你的API
+touch_api = "https://api.wer.plus/api/ruad?url=" # 请修改成你的摸头API
 menu = f'''使用方法:
 /摸 QQ号
 ''' # 如果只是/摸 时，发送这段消息
@@ -22,7 +22,7 @@ async def meme(qqid, type) -> None:
             return "很抱歉，由于技术限制，暂时不能这么用，请直接输入您的QQ号。"
         else:
             if type == "摸" or type == "touch":
-                api = api_url+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
+                api = touch_api+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
                 response = requests.get(api)
                 message = json.loads(response.text)
                 if message['code'] == 429:
@@ -32,7 +32,7 @@ async def meme(qqid, type) -> None:
                 return message['url']
             #暂未实现 吃
             elif type == "吃" or type == "eat":
-                api = api_url+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
+                api = touch_api+ f"https://qlogo4.store.qq.com/qzone/{qqid}/{qqid}/100"
                 response = requests.get(api)
                 message = json.loads(response.text)
                 if message['code'] == 429:
