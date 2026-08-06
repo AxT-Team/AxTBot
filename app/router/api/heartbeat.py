a37a58270f08643df579ee1e1ffe72a1b9fa9099
Shanshui2024 <shanshui@shanshui.site>
2026-08-06 15:41:01 +0800

    chore: 更新文档要求
    计划更新：
    - 【框架】把框架核心做成Pypi包导入
    - 【框架】新增脚手架快速创建
    - 【框架】新增热重载和DEBUG模式
    - 【框架】预留系统接口
    - 【QQ】将QQ相关代码转换为适配器
    - 【QQ】新增Websocket适配
    - 【协议】Milky协议适配

--- app/router/api/heartbeat.py
+++ app/router/api/heartbeat.py
@@ -14,5 +14,5 @@ async def heartbeat():
     GET方式请求心跳接口会有相关回包，可用于确保框架是否在线
     """
     return JSONResponse(
-        content={"code": 200, "message": "I'm here~"}
+        content={"code": 200, "message": "I'm online!"}
     )
